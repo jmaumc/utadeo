adc2190c03c905aad40c4915ddf68c63e1dc511b
Mauricio Mejia Castro <jmauriciomejia@protonmail.com>
2022-05-08 10:37:27 -0500

    Advances in stochastic simulation proyect proposal.

---
+++ stochastic-simulation/workshops/ws01/p3.R
@@ -0,0 +1,18 @@
+random_uniform_congruential <- function(n, x0, a, c, m) {
+  x <- 1:n
+  for (i in 1:n){
+    
+    if (i==1) {
+      x[i] <- (a*x0+c) %% m
+    }
+    else {
+      x[i] <- (a*x[(i-1)]+c) %% m
+    }
+    
+  }
+  return(x)
+}
+
+for (j in 0:8) {
+  print(random_uniform_congruential(20, j, 8, 3, 11))
+}
